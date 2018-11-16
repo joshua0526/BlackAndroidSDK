c352d001cc0a5edd93ab1cbf4436885b85f45745
lizhan <joshua0526@163.com>
2018-11-16 20:41:21 +0800

    修改makeRawTransaction错误

--- app/src/main/java/com/funjumping/blacat/MainActivity.java
+++ app/src/main/java/com/funjumping/blacat/MainActivity.java
@@ -141,5 +141,16 @@ public class MainActivity extends AppCompatActivity implements IResultListener,W
         BlaCatSDK.getBlaCatSDK().BlaCatSDK("setDefaultNetType", list1);
 
         BlaCatSDK.getBlaCatSDK().BlaCatSDK("LoginSDK");
+
+        List<String> sbParamJson = new ArrayList<>();
+        sbParamJson.add("(addr)AYkiQ74FHWFygR39WizXCz9f4xCLRYCxMT");
+        sbParamJson.add("(address)AWPVmAobCJGxrupvQSnovofakaVb2ue65a");
+        sbParamJson.add("(integer)100000");
+        List<String> list2 = new ArrayList<>();
+        list.add(sbParamJson.toString());
+        list.add("transfer");
+        list.add("0x3f7420285874867c30f32e44f304fd62ad1e9573");
+        list.add("makeRawTransaction");
+        BlaCatSDK.getBlaCatSDK().BlaCatSDK("makeRawTransaction", list2);
     }
 }
